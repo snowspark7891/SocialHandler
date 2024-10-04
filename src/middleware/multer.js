14763410c11af5ae98ab647aaf0bb95f3d3f6d7c
snowspark7891 <someshkumar7891@gmail.com>
2024-10-04 11:48:53 +0530

    File upload and multer .

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,2 +1,14 @@
-import { multer } from 'multer';
+import { multer } from 'multer';                      // taken from git hub diskstorage multer documentation
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp/")       //to store all the files
+    },
+    filename: function (req, file, cb) {
+     
+      cb(null, file.fieldname + '-'+destination+"-"+originalname)
+    }
+  })
+  
+ export const upload = multer({ 
+    storage: storage })
